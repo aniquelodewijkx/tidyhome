a0ce32aa5a7ed293cd18a8efd65ac7379b7ce69c
aniquelodewijkx1 <anique@hadrian.io>
2025-03-10 20:20:57 +0100

    add entrypoint

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version="0.1.0",
     packages=["tidyhome"],
     install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "tidyhome = tidyhome.cli:main"
+        ]
+    }
 )
