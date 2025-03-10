48ad900bd2f872d5c5283269777997db3a530014
aniquelodewijkx1 <anique@hadrian.io>
2025-03-10 20:25:09 +0100

    add dependency

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name="tidyhome",
     version="0.1.0",
     packages=["tidyhome"],
-    install_requires=[],
+    install_requires=[
+        "inquirer"
+    ],
     entry_points={
         "console_scripts": [
-            "tidyhome = tidyhome.cli:main"
+            "tidyhome = tidyhome.tidier:main"
         ]
     }
 )
